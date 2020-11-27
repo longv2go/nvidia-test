67ff29b533520ccc6d874fb4f8adaad6ec11f316
longv <longv2j@gmail.com>
2020-11-27 11:14:15 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,3 +8,6 @@ gcc -g -O0 -fPIC ${INCLUDE_DIRS} nvenc.c -lnvidia-encode -lcuda -o nvenc
 
 ## cuda devices info
 # g++ -g -O0 -fPIC ${INCLUDE_DIRS} -L${LIB_DIRS} cudadevices.cpp -lcuda -lcudart -o cudadevices
+
+## cuda add
+nvcc cuda_add.cu
