92a8a951270f056814f336a5bb01441830b3cf39
longv <longv2j@gmail.com>
2020-11-27 11:12:22 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+INCLUDE_DIRS="-I$(pwd)/../Video_Codec_SDK_11.0.10/Interface -I/usr/local/cuda/include"
+LIB_DIRS="/usr/local/cuda/lib64"
+
+## nvenc 
+gcc -g -O0 -fPIC ${INCLUDE_DIRS} nvenc.c -lnvidia-encode -lcuda -o nvenc
+
+## cuda devices info
+# g++ -g -O0 -fPIC ${INCLUDE_DIRS} -L${LIB_DIRS} cudadevices.cpp -lcuda -lcudart -o cudadevices
