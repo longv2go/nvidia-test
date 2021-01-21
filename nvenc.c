233d21fe387a545378c0d2deda611855b17239d6
longv <longv2j@gmail.com>
2021-01-21 10:28:58 +0800

    up nvenc

--- nvenc.c
+++ nvenc.c
@@ -1,9 +1,3 @@
-#include "nvEncodeAPI.h"
-#include <stdio.h>
-#include <dlfcn.h>
-#include <cuda.h>
-#include <string.h>
-
 /* tools 使用
  *  ffmpeg 截取一帧数据
  *  ffmpeg -i ba.mp4 -frames 1 -c:v rawvideo -pix_fmt nv12 out.yuv
@@ -15,6 +9,28 @@
  *  ffplay -f h264 -i test.264
  */
 
+#include "nvEncodeAPI.h"
+#include <stdio.h>
+#include <dlfcn.h>
+#include <cuda.h>
+#include <string.h>
+
+#define CK(call) do {\
+    NVENCSTATUS ret = call;\
+    if (NV_ENC_SUCCESS != (ret)) {\
+        printf( "[" #call "] call failed, error code: %d, line: %d\n", ret, __LINE__);\
+        exit(-1);\
+    }\
+} while(0);
+
+#define cuCK(call) do {\
+    CUresult ret = call;\
+    if (CUDA_SUCCESS != (ret)) {\
+        printf( "[" #call "] cuda function call failed, error code: %d, line: %d\n", ret, __LINE__);\
+        exit(-2);\
+    }\
+} while(0);
+
 #define INPUT_FILE_NAME "nv12.yuv"
 #define IN_FILE_PATH "/root/albert/" INPUT_FILE_NAME
 #define OUT_FILE_PATH "/root/albert/out_" INPUT_FILE_NAME ".h264"
@@ -24,47 +40,28 @@ static void *_nvencoder = NULL;
 static CUcontext _cuctx = NULL;
 
 void load_nvenc() {
-    NVENCSTATUS status = NvEncodeAPICreateInstance(&_nvenc);
-    if (status != NV_ENC_SUCCESS) {
-        printf("load nvenc lib failed!\n");
-        exit(1);
-    }
+    CK(NvEncodeAPICreateInstance(&_nvenc));
     printf("create interface success!\n");
 
-    CUresult result = cuInit(0);
-    if (result != CUDA_SUCCESS) {
-        printf("cuda init failed!\n");
-        exit(-1);
-    }
+    cuCK(cuInit(0));
 
     CUdevice cu_device;
-    result = cuDeviceGet(&cu_device, 0);
-    if (result != NV_ENC_SUCCESS) {
-        exit(11);
-    }
+    cuCK(cuDeviceGet(&cu_device, 0));
     printf("get device %d\n", cu_device);
 
     char szDeviceName[80];
-    result = cuDeviceGetName(szDeviceName, sizeof(szDeviceName), cu_device);
-    printf("%d, gpu name: %s\n", result, szDeviceName);
+    cuCK(cuDeviceGetName(szDeviceName, sizeof(szDeviceName), cu_device));
+    printf("gpu name: %s\n", szDeviceName);
 
-    result = cuCtxCreate(&_cuctx, 0, cu_device);
-    if (result != CUDA_SUCCESS) {
-        printf("cuda context create failed! %d\n", result);
-        exit(2);
-    }
+    cuCK(cuCtxCreate(&_cuctx, 0, cu_device));
 
     uint32_t version = 0;
     uint32_t currentVersion = (NVENCAPI_MAJOR_VERSION << 4) | NVENCAPI_MINOR_VERSION;
-    status = NvEncodeAPIGetMaxSupportedVersion(&version);
-    if (status != NV_ENC_SUCCESS) {
-        printf("get encode version failed!\n");
-        exit(-2);
-    }
+    CK(NvEncodeAPIGetMaxSupportedVersion(&version));
     printf("%d - %d (%d - %d)\n", version, currentVersion, NVENCAPI_MAJOR_VERSION, NVENCAPI_MINOR_VERSION);
 
     if (currentVersion > version) {
-        printf("version mismatch!\n");
+        printf("nvenc api version mismatch!\n");
         exit(-3);
     }
 
@@ -73,35 +70,23 @@ void load_nvenc() {
     encodeSessionExParams.device = _cuctx;
     encodeSessionExParams.deviceType = NV_ENC_DEVICE_TYPE_CUDA;
     encodeSessionExParams.apiVersion = NVENCAPI_VERSION;
-    status =  _nvenc.nvEncOpenEncodeSessionEx(&encodeSessionExParams, &_nvencoder);
-    if (status != NV_ENC_SUCCESS) {
-        printf("open encoder faield!, %d\n", status);
-        exit(3);
-    }
+    CK(_nvenc.nvEncOpenEncodeSessionEx(&encodeSessionExParams, &_nvencoder));
 
     printf("open encode session success!\n");
 }
 
 int print_info() {
-    NVENCSTATUS status;
     uint32_t first_count, second_count;
     GUID* guids = NULL;
 
     // Encode GUIDs
     printf("\n");
-    status = _nvenc.nvEncGetEncodeGUIDCount(_nvencoder, &first_count);
-    if (status != NV_ENC_SUCCESS) {
-        printf("nvEncGetEncodeGUIDCount failed, %d\n", status);
-        return -1;
-    }
+    CK(_nvenc.nvEncGetEncodeGUIDCount(_nvencoder, &first_count));
 
     guids = (GUID*)malloc(first_count * sizeof(GUID));
     uint32_t GUIDCount = 0;
-    status = _nvenc.nvEncGetEncodeGUIDs(_nvencoder, guids, first_count, &second_count);
-    if (status != NV_ENC_SUCCESS) {
-        printf("NvEncGetEncodeGUIDs failed\n");
-        return -1;
-    }
+    CK(_nvenc.nvEncGetEncodeGUIDs(_nvencoder, guids, first_count, &second_count));
+ 
     printf("Encode GUIDs: %d\n", second_count);
     for (int i = 0; i < second_count; i++) {
         printf("\t0x%x\n", guids[i].Data1);
@@ -110,18 +95,9 @@ int print_info() {
 
     // inputformat 
     printf("\n");
-    status = _nvenc.nvEncGetInputFormatCount(_nvencoder, NV_ENC_CODEC_H264_GUID, &first_count);
-    if (status != NV_ENC_SUCCESS) {
-        printf("get input format count failed!\n");
-        return -1;
-    }
-
+    CK(_nvenc.nvEncGetInputFormatCount(_nvencoder, NV_ENC_CODEC_H264_GUID, &first_count));
     NV_ENC_BUFFER_FORMAT* inputFmts = malloc(first_count * sizeof(NV_ENC_BUFFER_FORMAT));
-    status = _nvenc.nvEncGetInputFormats(_nvencoder, NV_ENC_CODEC_H264_GUID, inputFmts, first_count, &second_count);
-    if (status != NV_ENC_SUCCESS) {
-        printf("get input format info failed\n");
-        return -1;
-    }
+    CK(_nvenc.nvEncGetInputFormats(_nvencoder, NV_ENC_CODEC_H264_GUID, inputFmts, first_count, &second_count));
 
     printf("Get Input Formats: %d\n", second_count);
     for (int i = 0; i < second_count; i++) {
@@ -131,34 +107,20 @@ int print_info() {
 
     // preset GUIDs
     printf("\n");
-    status = _nvenc.nvEncGetEncodePresetCount(_nvencoder, NV_ENC_CODEC_H264_GUID, &first_count);
-    if (status != NV_ENC_SUCCESS) {
-        exit(2);
-    }
-
+    CK(_nvenc.nvEncGetEncodePresetCount(_nvencoder, NV_ENC_CODEC_H264_GUID, &first_count));
     guids = malloc(sizeof(GUID) * first_count);
-    status = _nvenc.nvEncGetEncodePresetGUIDs(_nvencoder, NV_ENC_CODEC_H264_GUID, guids, first_count, &second_count);
-    if (status != NV_ENC_SUCCESS) {
-        exit(2);
-    }
+    CK(_nvenc.nvEncGetEncodePresetGUIDs(_nvencoder, NV_ENC_CODEC_H264_GUID, guids, first_count, &second_count));
     printf("Preset GUIDs: %d\n", second_count);
     for (int i = 0; i < second_count; i++) {
         printf("\t0x%x\n", guids[i].Data1);
     }
     free(guids);
 
-    
     // Profile GUIDs
     printf("\n");
-    status = _nvenc.nvEncGetEncodeProfileGUIDCount(_nvencoder, NV_ENC_CODEC_H264_GUID, &first_count);
-    if (status != NV_ENC_SUCCESS) {
-        exit(2);
-    }
+    CK(_nvenc.nvEncGetEncodeProfileGUIDCount(_nvencoder, NV_ENC_CODEC_H264_GUID, &first_count));
     guids = malloc(sizeof(GUID) * first_count);
-    status = _nvenc.nvEncGetEncodeProfileGUIDs(_nvencoder, NV_ENC_CODEC_H264_GUID, guids, first_count, &second_count);
-    if (status != NV_ENC_SUCCESS) {
-        exit(2);
-    }
+    CK(_nvenc.nvEncGetEncodeProfileGUIDs(_nvencoder, NV_ENC_CODEC_H264_GUID, guids, first_count, &second_count));
     printf("profile GUIDs: %d\n", second_count);
     for (int i = 0; i < second_count; i++) {
         printf("\t0x%x\n", guids[i].Data1);
@@ -173,8 +135,7 @@ int print_info() {
 
 #define QUERY_CAP(cap) \
     capsParam.capsToQuery = cap; \
-    status = _nvenc.nvEncGetEncodeCaps(_nvencoder, NV_ENC_CODEC_H264_GUID, &capsParam, &capsVal); \
-    if (status != NV_ENC_SUCCESS) { exit(2); } \
+    CK(_nvenc.nvEncGetEncodeCaps(_nvencoder, NV_ENC_CODEC_H264_GUID, &capsParam, &capsVal)); \
     printf("\t" #cap ": %d\n", capsVal);
 
     QUERY_CAP(NV_ENC_CAPS_SUPPORT_YUV444_ENCODE);
@@ -197,8 +158,8 @@ int print_info() {
     QUERY_CAP(NV_ENC_CAPS_SUPPORT_LOOKAHEAD);
     QUERY_CAP(NV_ENC_CAPS_SUPPORT_MEONLY_MODE);
     QUERY_CAP(NV_ENC_CAPS_SUPPORT_BFRAME_REF_MODE);
-    QUERY_CAP(NV_ENC_CAPS_NUM_ENCODER_ENGINES);
     QUERY_CAP(NV_ENC_CAPS_SUPPORT_FIELD_ENCODING);
+    QUERY_CAP(NV_ENC_CAPS_NUM_ENCODER_ENGINES);
 }
 
 void print_enc_config(NV_ENC_CONFIG config) {
@@ -229,11 +190,7 @@ void init_encoder() {
     
     // 一定要设置 encodeConfig
     NV_ENC_PRESET_CONFIG presetConfig = { NV_ENC_PRESET_CONFIG_VER, { NV_ENC_CONFIG_VER } };
-    int status = _nvenc.nvEncGetEncodePresetConfigEx(_nvencoder, NV_ENC_CODEC_H264_GUID, NV_ENC_PRESET_P4_GUID, NV_ENC_TUNING_INFO_LOW_LATENCY, &presetConfig);
-    if (status != NV_ENC_SUCCESS) {
-        printf("get preset config failed! %d\n", status);
-        exit(2);
-    }
+    CK(_nvenc.nvEncGetEncodePresetConfigEx(_nvencoder, NV_ENC_CODEC_H264_GUID, NV_ENC_PRESET_P4_GUID, NV_ENC_TUNING_INFO_LOW_LATENCY, &presetConfig));
     printf("\nget preset config success， %p\n", params.encodeConfig);
     print_enc_config(presetConfig.presetCfg);
 
@@ -243,32 +200,22 @@ void init_encoder() {
     // params.encodeConfig->frameIntervalP = 0;
     // params.encodeConfig->frameFieldMode = NV_ENC_PARAMS_FRAME_FIELD_MODE_FRAME;
 
-    status = _nvenc.nvEncInitializeEncoder(_nvencoder, &params);
-    if (status != NV_ENC_SUCCESS) {
-        printf("init encoder failed! %d\n", status);
-        exit(1);
-    }
+    CK(_nvenc.nvEncInitializeEncoder(_nvencoder, &params));
+
     printf("init encoder success!\n");
 }
 
 void end_encode() {
-    int status;
-    
     // 通知 encoder 输入结束
     NV_ENC_PIC_PARAMS params = { 0 };
     params.version = NV_ENC_PIC_PARAMS_VER;
     params.encodePicFlags = NV_ENC_PIC_FLAG_EOS;
-    status = _nvenc.nvEncEncodePicture(_nvencoder, &params);
-    if (status != NV_ENC_SUCCESS) {
-        exit(2);
-    }
+
+    CK(_nvenc.nvEncEncodePicture(_nvencoder, &params));
     printf("notify input stream end success!\n");
 
     // close encoder
-    status = _nvenc.nvEncDestroyEncoder(_nvencoder);
-    if (status != NV_ENC_SUCCESS) {
-        exit(2);
-    }
+    CK(_nvenc.nvEncDestroyEncoder(_nvencoder));
     printf("destroy encoder success!\n");
 
     // destroy cuda context
@@ -286,20 +233,12 @@ void encode() {
     inputbuf.bufferFmt = NV_ENC_BUFFER_FORMAT_NV12;
     inputbuf.inputBuffer = NULL;
 
-    int status = _nvenc.nvEncCreateInputBuffer(_nvencoder, &inputbuf);
-    if (status != NV_ENC_SUCCESS) {
-        printf("create input buffer failed!\n");
-        exit(2);
-    }
+    CK(_nvenc.nvEncCreateInputBuffer(_nvencoder, &inputbuf));
     printf("create input buffer success, %p\n", inputbuf.inputBuffer);
 
     // create output stream
     NV_ENC_CREATE_BITSTREAM_BUFFER outbuf = { NV_ENC_CREATE_BITSTREAM_BUFFER_VER };
-    status = _nvenc.nvEncCreateBitstreamBuffer(_nvencoder, &outbuf);
-    if (status != NV_ENC_SUCCESS) {
-        printf("create output stream failed! %d\n", status);
-        exit(2);
-    }
+    CK(_nvenc.nvEncCreateBitstreamBuffer(_nvencoder, &outbuf));
     printf("create output stream success, %p\n", outbuf.bitstreamBuffer);
 
     // read yuv data
@@ -314,22 +253,14 @@ void encode() {
     lockBufferParams.inputBuffer = inputbuf.inputBuffer;
     lockBufferParams.doNotWait = 0;
     lockBufferParams.reservedBitFields = 0;
-    status = _nvenc.nvEncLockInputBuffer(_nvencoder, &lockBufferParams);
-    if (status != NV_ENC_SUCCESS) {
-        printf("lock input buffer failed! %d\n", status);
-        exit(2);
-    }
+    CK(_nvenc.nvEncLockInputBuffer(_nvencoder, &lockBufferParams));
     printf("lock success: %p, %d\n", lockBufferParams.bufferDataPtr, lockBufferParams.pitch);
 
     // copy yuv data to GPU
     fread(lockBufferParams.bufferDataPtr, 1, size, fp);
     printf("writer buffer size: %lu\n", size);
 
-    status = _nvenc.nvEncUnlockInputBuffer(_nvencoder, inputbuf.inputBuffer);
-    if (status != NV_ENC_SUCCESS) {
-        printf("unlock failed, %d\n", status);
-        exit(2);
-    }
+    CK(_nvenc.nvEncUnlockInputBuffer(_nvencoder, inputbuf.inputBuffer));
     printf("unlock success!\n");
 
     // construct pic params
@@ -348,11 +279,7 @@ void encode() {
     picParams.encodePicFlags = NV_ENC_PIC_FLAG_FORCEIDR;
 
     // submit frame to encode using GPU
-    status = _nvenc.nvEncEncodePicture(_nvencoder, &picParams);
-    if (status != NV_ENC_SUCCESS) {
-        printf("GPU encode failed! %d\n", status);
-        exit(3);
-    }
+    CK(_nvenc.nvEncEncodePicture(_nvencoder, &picParams));
     printf("GPU encode success\n");
 
     // process output stream
@@ -360,37 +287,24 @@ void encode() {
     bitstream.version = NV_ENC_LOCK_BITSTREAM_VER;
     bitstream.outputBitstream = outbuf.bitstreamBuffer;
 
-    status = _nvenc.nvEncLockBitstream(_nvencoder, &bitstream);
-    if (status != NV_ENC_SUCCESS) {
-        exit(2);
-    }
+    CK(_nvenc.nvEncLockBitstream(_nvencoder, &bitstream));
     printf("Total out bytes: %d\n", bitstream.bitstreamSizeInBytes);
     
     uint8_t *outdata = malloc(bitstream.bitstreamSizeInBytes);
     memcpy(outdata, bitstream.bitstreamBufferPtr, bitstream.bitstreamSizeInBytes);
 
-    status = _nvenc.nvEncUnlockBitstream(_nvencoder, outbuf.bitstreamBuffer);
-    if (status != NV_ENC_SUCCESS) {
-        exit(2);
-    }
+    CK(_nvenc.nvEncUnlockBitstream(_nvencoder, outbuf.bitstreamBuffer));
 
     // out ot file
     FILE *out_fp = fopen(OUT_FILE_PATH, "w");
     fwrite(outdata, 1, bitstream.bitstreamSizeInBytes, out_fp);
 
-
     // destroy input & output buffer
     printf("\ndestroying\n");
-    status = _nvenc.nvEncDestroyInputBuffer(_nvencoder,  picParams.inputBuffer);
-    if (status != NV_ENC_SUCCESS) {
-        exit(2);
-    }
+    CK(_nvenc.nvEncDestroyInputBuffer(_nvencoder,  picParams.inputBuffer));
     printf("destroy input buffer success!\n");
 
-    status = _nvenc.nvEncDestroyBitstreamBuffer(_nvencoder, picParams.outputBitstream);
-    if (status != NV_ENC_SUCCESS) {
-        exit(2);
-    }
+    CK(_nvenc.nvEncDestroyBitstreamBuffer(_nvencoder, picParams.outputBitstream));
     printf("destroy output stream success!\n");
 
     free(outdata);
